feaef56f331da38d3a122115178c9ca8e66301aa
Henry Smith <henry.smith@netlify.com>
2023-08-02 12:55:34 +0100

    Update build id

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  generateBuildId: async () => {
+    // You can, for example, get the latest git commit hash here
+    return process.env.NEXT_PUBLIC_LOCALE;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
