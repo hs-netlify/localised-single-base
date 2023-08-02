54b7d3063ac5cacbd2ae084d5428f06ff1fbf0bf
Henry Smith <henry.smith@netlify.com>
2023-08-02 13:13:59 +0100

    No build command

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  generateBuildId: async () => {
-    // You can, for example, get the latest git commit hash here
-    return process.env.NEXT_PUBLIC_LOCALE || "base";
-  },
+  // generateBuildId: async () => {
+  //   // You can, for example, get the latest git commit hash here
+  //   return process.env.NEXT_PUBLIC_LOCALE || "base";
+  // },
 };
 
 module.exports = nextConfig;
